662bf00d76be8956fa81946d35508aebac286898
GaganShemdi <gaganshemdi@icloud.com>
2026-04-19 22:51:29 +0530

    Using Spring Boot Beans adding "@Component" in email service ( Concreate class implemented the interface NotificationService ) and using "@Autowired" , injecting the bean and using it to create a object

--- src/main/java/com/springcourse/gagan/module1Introduction/Impl/SmsNotificationService.java
+++ src/main/java/com/springcourse/gagan/module1Introduction/Impl/SmsNotificationService.java
@@ -1,7 +1,11 @@
 package com.springcourse.gagan.module1Introduction.Impl;
 
 import com.springcourse.gagan.module1Introduction.NotificationService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Primary
+@Component
 public class SmsNotificationService implements NotificationService {
     @Override
     public void send(String message){
